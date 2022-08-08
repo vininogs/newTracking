5f416a4ed0b51155fc2b87bf3a8250c0e2604a2d
Vinicius <viniciusnogueiraf0@gmail.com>
2022-08-08 13:46:26 -0300

    New tracking

---
+++ timeline.js
@@ -0,0 +1,39 @@
+    $(".step").click( function() {
+        $(this).addClass("active").prevAll().addClass("active");
+        $(this).nextAll().removeClass("active");
+    });
+    
+    $(".step01").click( function() {
+        $("#line-progress").css("width", "3%");
+        $(".discovery").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step02").click( function() {
+        $("#line-progress").css("width", "25%");
+        $(".strategy").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step03").click( function() {
+        $("#line-progress").css("width", "50%");
+        $(".creative").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step04").click( function() {
+        $("#line-progress").css("width", "75%");
+        $(".production").addClass("active").siblings().removeClass("active");
+    });
+    
+    $(".step05").click( function() {
+        $("#line-progress").css("width", "100%");
+        $(".analysis").addClass("active").siblings().removeClass("active");
+    });
+    
+
+    //table
+
+    $(document).ready(function () {
+        $('#dtHorizontalExample').DataTable({
+          "scrollX": true
+        });
+        $('.dataTables_length').addClass('bs-select');
+      });
